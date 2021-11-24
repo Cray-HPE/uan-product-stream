28695affd905524630c5aa7761bc85dad1b04bf0
Jason Sollom <jason.sollom@hpe.com>
2021-11-24 11:19:17 -0600

    CASMUSER-2915: Change version from 2.1.0 to 2.1.8

--- include/nexus-upload.sh
+++ include/nexus-upload.sh
@@ -11,4 +11,4 @@ skopeo-sync "${ROOTDIR}/docker"
 nexus-upload helm "${ROOTDIR}/helm" "${CHARTS_REPO:-"charts"}"
 
 # Upload repository contents
-nexus-upload raw "${ROOTDIR}/rpms/cray-sles15-sp2-ncn" "uan-2.1.0-sle-15sp2"
+nexus-upload raw "${ROOTDIR}/rpms/cray-sles15-sp2-ncn" "uan-2.1.8-sle-15sp2"
