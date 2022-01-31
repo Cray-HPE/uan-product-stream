43682051d74fcc0bd9d286b46e341838d9fb787e
Jeff Keopp <77171187+keopp-hpe@users.noreply.github.com>
2022-01-31 12:03:36 -0600

    Incremental update

--- docs/hugo/docs-uan/bin/push.sh
+++ docs/hugo/docs-uan/bin/push.sh
@@ -6,7 +6,7 @@ mkdir -p docs-uan
 cd docs-uan
 git clone --depth=1 -b release/docs-html git@github.com:Cray-HPE/uan-product-stream.git
 cd ..
-rm -rf docs-uan/docs-uan/* docs-uan/docs-uan/.github docs-uan/docs-uan/.gitignore docs-uan/docs-uan/.version
+rm -rf docs-uan/docs-uan/* docs-uan/docs-uan/.gitignore
 cp -r public/* docs-uan/docs-uan/
 cd docs-uan/docs-uan
 git add .
