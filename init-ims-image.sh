ba534a2f0c46d79005398af0c5a90a1ea3d86612
Alan Mutschelknaus <81820210+alanm-hpe@users.noreply.github.com>
2022-08-05 13:52:40 -0500

    Update missing usage field and copyrights

--- init-ims-image.sh
+++ init-ims-image.sh
@@ -1,4 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# MIT License
+#
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
 
 CRAY_FORMAT=json
 
@@ -30,11 +53,12 @@ function usage() {
   echo "IMS will be calculated and registered to the appropriate IMS image ID. The resultant "
   echo "IMS image ID will be returned upon completion."
   echo ""
-  echo "Usage: $0 [-h|-b <s3 bucket>] -k <kernel-path> -i <initrd-path> -r <rootfs-path>"
+  echo "Usage: $0 [-h|-b <s3 bucket>] -n <image-name> -k <kernel-path> -i <initrd-path> -r <rootfs-path>"
   echo ""
   echo "options:"
   echo "h      Print this help"
   echo "v      verbose mode"
+  echo "n      name of the image"
   echo "k      path to a kernel"
   echo "i      path to an initrd"
   echo "r      path to a rootfs"
